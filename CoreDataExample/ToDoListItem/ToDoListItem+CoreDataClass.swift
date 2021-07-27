bb594fb2c47635688ba767ec7d6cde5d1009b2e7
KANTAMIZUNO <kanta2000127@icloud.com>
2021-07-27 16:21:41 +0900

    first commit

---
+++ CoreDataExample/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  CoreDataExample
+//
+//  Created by iOSDev on 2021/07/27.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
